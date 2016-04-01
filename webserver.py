9ad74ce4649c28a02b50b1a59cc3c614d8327413
vladimiriacobm@gmail.com <vladimiriacobm@gmail.com>
2016-04-01 17:14:16 +0300

    todo: make title indexing work

--- webserver.py
+++ webserver.py
@@ -8,8 +8,8 @@ db = client.homework
 
 app = Flask(__name__)
 
-def get_query(year = None, day = None, category = None):
-	if (not year and not day and not category): return "Hello ^_^"
+def get_query(category = None, year = None, day = None, info = None):
+	if (not year and not day and not category and not info): return "Hello ^_^"
 	loc = locals()
 	criteria = {k:loc[k] for k in loc if loc[k] != None}
 	return db.wiki.find(criteria, {'_id': False})
@@ -19,8 +19,9 @@ def get_req():
 	year = request.args.get('year')
 	day = request.args.get('day')
 	category = request.args.get('category')
+	title = request.args.get('title')
 
-	exp = get_query(year, day, category)
+	exp = get_query(category, year, day, title)
 	return dumps({'results': list(exp)},
 		ensure_ascii = False,
 		indent = 4).encode('utf8')
